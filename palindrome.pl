42bda61a88fd40f3fe7f7ec4e19a31c37e90381d
appfogtest8 <appfogtest8@mm.st>
2013-12-01 19:06:11 -0800

    init commit

---
+++ palindrome.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+print "? ";
+my $str = <STDIN>;
+chomp $str;
+
+while ((index $str, " ") != -1){
+	substr($str, (index $str, " ")) = substr $str, ((index $str, " ") + 1);
+	print $str;
+	print "\n";
+}
+
+if ((substr $str, 0, ((length $str) / 2)) == (scalar reverse (substr $str, ((length $str) / 2) + ((length $str) % 2)))) {
+	print "yes\n";
+} else {
+print "no\n";
+}
